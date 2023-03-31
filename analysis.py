add84eefa0887451dd66341522c9fbadc2ff4e44
rsalgo <nur@bujang.com>
2023-03-31 12:42:28 +0100

    boxplot

--- analysis.py
+++ analysis.py
@@ -57,7 +57,7 @@ df=pd.read_csv('iris.data', names=columns) # read the csv file and assign each c
 # 2. Histogram of each variable into png file
 # Visualizing 4 histograms of each column is not very informative, so overlapping histogram is chosen
 # sns.set(style="whitegrid") # set background
-# fig,axs = plt.subplots(2,2, figsize = (7,8)) # set arrangement of subplots (2 down, 2 across), set size of whole figure (7 width, 8 height)
+# fig,axs = plt.subplots(2,2, figsize = (7,8)) # set grid position of subplots (2 down, 2 across), set size of whole figure (7 width, 8 height)
 # fig.suptitle('Petal and sepal dimensions of three Iris species', color ='#191970', fontweight='bold') # customize figure's main title
 # sns.histplot(data=df, x="Sepal length (cm)", kde=True, color="olive", ax=axs[0, 0]) # Kernel density estimation (KDE) smooths the replicates with a Gaussian kernel
 # sns.histplot(data=df, x="Sepal width (cm)", kde=True, color="green", ax=axs[0, 1]) # ax is the coordinate of each subplot on the figure
@@ -84,50 +84,69 @@ df=pd.read_csv('iris.data', names=columns) # read the csv file and assign each c
 # # The petal length and width of I. setosa have much narrower distribution compared to the other two species. 
 # # there are overlaps in sepal length and width of all three species.
 
-# 4. Other analysis
-# Perform Pearson correlation analysis to determine the degree of linear relationship between 2 continuous variables
-# correlation efficient closer to 1 indicates a strong +ve relationship, closer to -1 indicates a strong -ve relationship
-corr = df.corr(method="pearson") # 
-bool_upper_matrix = np.tril(np.ones(corr.shape)).astype(bool) # eliminate upper triangle for better readibility
-corr = corr.where(bool_upper_matrix)
-print(corr)
-# Results:
-# High positive correlation between Petal length & Petal width, Petal length & Sepal length and Sepal length and petal width
+# # 4. Other analysis, Exploratory data analysis (visual techniques to detect outliers, trends/pattern)
+# # 4a. Perform Pearson correlation analysis to determine the degree of linear relationship between 2 continuous variables
+# # correlation efficient closer to 1 indicates a strong +ve relationship, closer to -1 indicates a strong -ve relationship
+# corr = df.corr(method="pearson") # 
+# bool_upper_matrix = np.tril(np.ones(corr.shape)).astype(bool) # eliminate upper triangle for better readibility
+# corr = corr.where(bool_upper_matrix)
+# print(corr)
+# # Results:
+# # High positive correlation between Petal length & Petal width, Petal length & Sepal length and Sepal length and petal width
 
-# A Correlation matrix to easily visualize the parameters which best correlate with each other
-fig, ax = plt.subplots(figsize=(9,6)) # set size of whole figure (11 width, 6 height)
-fig.suptitle('Correlation matrix of petal and sepal of three Iris species', color ='#191970', fontweight='bold') # customize figure's main title
-# customize heatmap
-# Adjust the axes attribute to “equal” if True so that each cell gets square-shaped.
-# for color bar, default is vertical, but to move it to the bottom, just add 'orientation': 'horizontal' to cbar argument
-h=sns.heatmap(corr, annot=True, ax=ax, cmap = 'coolwarm', square=True, linewidths = 0.1, linecolor='yellow', cbar_kws={'label': 'range', 'shrink': 0.9}) 
-h.set_xticklabels(h.get_xticklabels(), rotation = 0, fontsize = 10) # This sets the xticks "upright" as opposed to sideways in any figure size
-h.set_yticklabels(h.get_yticklabels(),rotation = 0, fontsize = 10) # This sets the yticks "upright" in any figure size
+# # 4.2 A Correlation matrix to easily visualize the parameters which best correlate with each other
+# fig, ax = plt.subplots(figsize=(9,6)) # set size of whole figure (11 width, 6 height)
+# fig.suptitle('Correlation matrix of petal and sepal of three Iris species', color ='#191970', fontweight='bold') # customize figure's main title
+# # customize heatmap
+# # Adjust the axes attribute to “equal” if True so that each cell gets square-shaped.
+# # for color bar, default is vertical, but to move it to the bottom, just add 'orientation': 'horizontal' to cbar argument
+# h=sns.heatmap(corr, annot=True, ax=ax, cmap = 'coolwarm', square=True, linewidths = 0.1, linecolor='yellow', cbar_kws={'label': 'range', 'shrink': 0.9}) 
+# h.set_xticklabels(h.get_xticklabels(), rotation = 0, fontsize = 10) # This sets the xticks "upright" as opposed to sideways in any figure size
+# h.set_yticklabels(h.get_yticklabels(),rotation = 0, fontsize = 10) # This sets the yticks "upright" in any figure size
+# plt.show() # show plot
+# # results show a strong positive correlation between Petal length & Petal width, Petal length & Sepal length, Sepal length & Petal width
+
+# # 4.3 If I group by species:
+# df.groupby("Iris species").corr(method="pearson")
+# print (df.groupby("Iris species").corr(method="pearson"))
+# # Results:
+# # Iris setosa: high correlation between Sepal length & Sepal width
+# # Iris versicolor: strong correlation between Petal length & Petal width, Petal length & Sepal length
+# # Iris virginica: high correlation between Petal length & Sepal length
+
+# # 4.4 Box plot
+def graph(y): # define graph of Iris species as y axis
+    sns.boxplot(x="Iris species", y=y, data=df)
+    sns.stripplot(x="Iris species", y=y, data=df, jitter=True, edgecolor="gray", alpha=0.35)# add stripplot/jitter plot, set transparency (alpha)
+plt.figure(figsize=(10,10))
+plt.subplot(221) # grid position top left (2 rows, 2 columns, first top)
+graph('Sepal length (cm)')
+plt.subplot(222) # grid position top right (2 rows, 2 columns, second top)
+graph('Sepal width (cm)')
+plt.subplot(223) # grid position bottom left (2 rows, 2 columns, first bottom)
+graph('Petal length (cm)')
+plt.subplot(224) # grid position bottom right (2 rows, 2 columns, second bottom)
+graph('Petal width (cm)')
 plt.show() # show plot
-# results show a strong positive correlation between Petal length & Petal width, Petal length & Sepal length, Sepal length & Petal width
-
-# if I group by species:
-df.groupby("Iris species").corr(method="pearson")
-print (df.groupby("Iris species").corr(method="pearson"))
 # Results:
-# Iris setosa: high correlation between Sepal length & Sepal width
-# Iris versicolor: strong correlation between Petal length & Petal width, Petal length & Sepal length
-# Iris virginica: high correlation between Petal length & Sepal length
+# Iris setosa has the least distributed and smallest petal size
+# Iris virginica has the biggest petal size, and Iris versicolor's petal size is between Iris setosa and virginica
+# Sepal size may not be a good variable to differentiate species
+
+# Decision Tree classification
+
+# split the data into training (80%) and testing (20%) to detect overfitting (model learned the training data very well but fails on testing)
+from sklearn.model_selection import train_test_split # to split the dataset into training and testing
+X = df.iloc[:,:2] # store the first two columns (Sepal length and Sepal width) in an array X 
+y = df.iloc[:,4] # store the target variable as label into an array y
+print(X.shape, y.shape) # display number of rows and columns
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 42) # split the dataset into training (80%) and testing (20%)
+# random_state is the seed of randomness to help reproduce the same results everytime
+print(X_train.shape, X_test.shape, y_train.shape, y_test.shape) # display the shape and label of training and testing set
 
 
 
-# Exploratory data analysis - visual techniques to detect outliers, trends/pattern
 
-
-
-# # split the data into training (80%) and testing (20%) to detect overfitting (model learned the training data very well but fails on testing)
-# from sklearn.model_selection import train_test_split # to split the dataset into training and testing
-# X = df.iloc[:,:2] # store the first two columns (Sepal length and Sepal width) in an array X 
-# y = df.iloc[:,4] # store the target variable as label into an array y
-# print(X.shape, y.shape) # display number of rows and columns
-# X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 42) # split the dataset into training (80%) and testing (20%)
-# # random_state is the seed of randomness to help reproduce the same results everytime
-# print(X_train.shape, X_test.shape, y_train.shape, y_test.shape) # display the shape and label of training and testing set
 
 
 
